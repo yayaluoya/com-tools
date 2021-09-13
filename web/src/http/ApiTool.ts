0449ea4680c8f99e688fd3db52d7fe0216437ac5
yayaluoya <yayaluoya@sina.com>
2021-09-13 11:21:11 +0800

    -

--- web/src/http/ApiTool.ts
+++ web/src/http/ApiTool.ts
@@ -13,34 +13,43 @@ export class ApiTool {
      * @param  {...any} args 参数 
      */
     static getApi(domainName: string, keyWorlds: string, ...args: string[]): string {
-        let domainPath = ApiConfig.domainPath[domainName];
-        if (!domainPath) {
-            console.error('没有找到api域', domainName);
-            return;
-        }
-        domainPath = domainPath.replace(/\/+$/, '');
-
         let url: string = ApiConfig.apiPath as any;
         for (let _o of keyWorlds.split('.')) {
             url = url[_o];
             //
             if (typeof url == 'undefined' || url == null) {
                 console.error('找不到api层级', _o);
-                return domainPath;
+                return '';
             }
         }
 
         if (url && typeof url == 'string') {
-            url = url.replace(/^\//, '').replace(/\/+/g, '/');
             for (let i = 0; i < args.length; i++) {
                 url = url.replace(url.match(pathArgsReg)[i], args[i]);
             }
             //组合路径
-            return `${domainPath}/${url}`;
+            return this.combinationApi(domainName, url);
         } else {
             console.error(`没有找到 ${keyWorlds} 配置的api`);
             //
-            return domainPath;
+            return '';
         }
+    }
+
+    /**
+     * 组合api
+     * @param {*} domainName 域
+     * @param {*} url api
+     */
+    static combinationApi(domainName: string, url: string): string {
+        let domainPath = ApiConfig.domainPath[domainName];
+        if (!domainPath) {
+            console.error('没有找到api域', domainName);
+            return;
+        }
+        domainPath = domainPath.replace(/\/+$/, '');
+        url = url.replace(/\\+/, '/').replace(/^\//, '').replace(/\/+/g, '/');
+        //
+        return `${domainPath}/${url}`;
     }
 }
