191b979b6b8cfa3df2245fce407617742e65af15
yayaluoya <yayaluoya@sina.com>
2022-08-22 16:35:29 +0800

    添加一个分页的工具

---
+++ src/web/Pagination.ts
@@ -0,0 +1,60 @@
+/**
+ * 分页对象
+ */
+export class Pagination {
+    /** 总数 */
+    total = 0;
+    /** 当前页面 */
+    currentPage = 1;
+    /** 分页大小 */
+    pageSize = 0;
+    /** 可变分页大小 */
+    pageSizes: number[] = [];
+
+    /** 是否是最后一页 */
+    get lastPage() {
+        return this.total <=
+            this.currentPage * this.pageSize;
+    }
+    /** 最大页 */
+    get maxPage() {
+        return Math.ceil(this.total / this.pageSize);
+    }
+
+    constructor(number = 10, numbers: number[] = []) {
+        this.pageSize = number;
+        this.pageSizes = numbers;
+    }
+
+    /**
+     * 分页递增
+     * @param n 
+     */
+    add(n = 1) {
+        n = this.currentPage + n;
+        this.currentPage = Math.min(Math.max(0, n), this.maxPage);
+    }
+
+    /**
+     * 获取分页op，主要是调接口时用的到
+     * TODO 根据后端习惯重写此接口
+     * @param query 
+     * @returns 
+     */
+    getPageOp(query = {}) {
+        return {
+            pageSize: this.pageSize,
+            currentPage: this.currentPage,
+            ...query,
+        };
+    }
+
+    /**
+     * 获取序号
+     * @param {*} index 索引 从0开始的这个
+     * @param {*} pagination 当前页面的分页信息
+     */
+    static getIndex(index: number, pagination: Pagination) {
+        return index + 1 + (pagination.currentPage - 1) * pagination.pageSize;
+    }
+};
