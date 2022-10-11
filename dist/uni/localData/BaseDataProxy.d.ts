9ddceb7561297981cec8506bed06f519b11713cc
yayaluoya <yayaluoya@sina.com>
2022-10-11 11:28:47 +0800

    关于各平台BaseDataProxy的类型问题

--- dist/uni/localData/BaseDataProxy.d.ts
+++ dist/uni/localData/BaseDataProxy.d.ts
@@ -3,7 +3,7 @@ import { LocalStorage_ } from "./LocalStorage_";
 /**
  * 基类本地数据代理
  */
-export declare abstract class BaseDataProxy<D = any> extends BaseDataProxy_ {
+export declare abstract class BaseDataProxy<D = any> extends BaseDataProxy_<D> {
     get LocalStorage_(): typeof LocalStorage_;
 }
 //# sourceMappingURL=BaseDataProxy.d.ts.map
--- src/node/localData/BaseDataProxy.ts
+++ src/node/localData/BaseDataProxy.ts
@@ -4,7 +4,7 @@ import { LocalStorage_ } from "./LocalStorage_";
 /**
  * 基类本地数据代理
  */
-export abstract class BaseDataProxy<D = any> extends BaseDataProxy_ {
+export abstract class BaseDataProxy<D = any> extends BaseDataProxy_<D> {
     get LocalStorage_() {
         return LocalStorage_;
     }
