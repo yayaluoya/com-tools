1af7ce628f4580507d900082fb97fc4f18dbe42e
yayaluoya <yayaluoya@sina.com>
2023-04-28 09:59:24 +0800

    -

--- src/web/event/GlobalE.ts
+++ src/web/event/GlobalE.ts
@@ -5,8 +5,8 @@ import { instanceTool } from "../../instanceTool";
  * 全局事件
  */
 @instanceTool()
-export class GlobalE<T extends string> extends BaseEvent<T> {
+export class GlobalE<T extends string = string> extends BaseEvent<T> {
     /** 单例 */
-    static readonly instance: GlobalE<string>;
-    static readonly I: GlobalE<string>;
+    static readonly instance: GlobalE;
+    static readonly I: GlobalE;
 }
