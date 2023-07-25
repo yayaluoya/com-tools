16c9f13dd5f172455c2da81a6da0dd08a85642d2
yayaluoya <yayaluoya@sina.com>
2023-07-25 11:05:05 +0800

    获取apipost的列表工具，这个工具的意义就是自动生成api代码

--- test/src/node/index.js
+++ test/src/node/index.js
@@ -1,7 +1,8 @@
-
 console.log('node测试2');
+const fs = require('fs');
+const path = require('path');
 
-require('./http');
+// require('./http');
 
 require('./ObjectUtils');
 
@@ -10,4 +11,15 @@ require('./lookupFile');
 require('./HttpTool');
 require('./SocketManager');
 require('./server/server');
-require('./cmd');
+// require('./cmd');
+
+console.log('获取apipost列表');
+require('yayaluoya-tool/dist/node/getApipostList')
+    .getApipostList('b19ad72b1fe67014', '5b576e616b1149bb')
+    .then((data) => {
+        fs.writeFileSync(
+            path.join(__dirname, '../../dist/openapiList.json'),
+            JSON.stringify(data, undefined, 2),
+        );
+        console.log('获取apipost列表完成');
+    });
